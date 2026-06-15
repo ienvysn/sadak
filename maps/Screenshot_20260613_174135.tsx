50681b3bd5bfa80f1a9f5cf1b9bd4386b965b2d4
lavnik <ienvysnn@gmail.com>
2026-06-15 13:29:42 +0545

    added .tsx file

---
+++ maps/Screenshot_20260613_174135.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Screenshot_20260613_174135" tilewidth="1040" tileheight="1292" tilecount="1" columns="1">
+ <image source="Pictures/Screenshot_20260613_174135.png" width="1040" height="1292"/>
+</tileset>
